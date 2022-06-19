11b32bdacd1052cfe6709fdd2f0e833b0e339f41
ScorpionCallejas <scorpionmorales17@gmail.com>
2022-06-18 20:35:13 -0500

    Script para preguntas

--- js/script.js
+++ js/script.js
@@ -1,10 +1,49 @@
 // Cargar pregunta
-loadQuestion(0)
+let indexQuestion = 0;
+let score = 0;
 
-function loadQuestion(index){
-    let objectQuestion = baseQuestions[index]
-    let options = [...objectQuestion.options]
-    options.push(objectQuestion.response)
-    document.getElementById("question").innerHTML = objectQuestion.Question
-    document.getElementById("option-1").innerHTML = objectQuestion.Question
+loadQuestion(indexQuestion);
+
+function loadQuestion(index) {
+  objectQuestion = baseQuestions[index];
+  options = [...objectQuestion.Options];
+  options.push(objectQuestion.response);
+  for (let i = 0; i < 4; i++) {
+    options.sort(() => Math.random() - 0.5);
+  }
+  document.getElementById("question").innerHTML = objectQuestion.question;
+  document.getElementById("img-question").src = objectQuestion.image;
+  document.getElementById("option-1").innerHTML = options[0];
+  document.getElementById("option-2").innerHTML = options[1];
+  document.getElementById("option-3").innerHTML = options[2];
+  document.getElementById("option-4").innerHTML = options[3];
+}
+
+async function selectionResponse(index) {
+  let correctResponse = options[index] == objectQuestion.response;
+  if (correctResponse) {
+    await Swal.fire({
+      title: "Respuesta correcta",
+      text: "La respuesta ha sido correcta",
+      icon: "success",
+    });
+    score++;
+  } else {
+    await Swal.fire({
+      title: "Respuesta incorrecta",
+      text: "Estudia un poco más",
+      icon: "error",
+    });
+  }
+
+  indexQuestion++;
+  if (indexQuestion >= baseQuestions.length) {
+    await Swal.fire({
+      title: "Juego terminado",
+      text: `Tu puntaje fue de: "${score}/${baseQuestions.length}"`,
+    });
+    indexQuestion = 0;
+    score = 0;
+  }
+  loadQuestion(indexQuestion);
 }
